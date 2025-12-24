1cca843bbe44a63e756e9006ee9d67ebd5ca32a0
ankol <2329340232@qq.com>
2025-12-24 22:35:51 +0800

    Work in progress

--- src/main/java/ankol/mod/merger/merger/xml/TechlandXmlFileVisitor.java
+++ src/main/java/ankol/mod/merger/merger/xml/TechlandXmlFileVisitor.java
@@ -2,8 +2,11 @@ package ankol.mod.merger.merger.xml;
 
 import ankol.mod.merger.antlr.xml.TechlandXMLParser;
 import ankol.mod.merger.antlr.xml.TechlandXMLParserBaseVisitor;
-import ankol.mod.merger.merger.xml.node.XmlElementNode;
 import ankol.mod.merger.merger.xml.node.XmlNode;
+import org.antlr.v4.runtime.RuleContext;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TechlandXmlFileVisitor extends TechlandXMLParserBaseVisitor<XmlNode> {
     public static final String ELEMENT = "element";
@@ -15,8 +18,20 @@ public class TechlandXmlFileVisitor extends TechlandXMLParserBaseVisitor<XmlNode
 
     @Override
     public XmlNode visitElement(TechlandXMLParser.ElementContext ctx) {
-        var signature = ELEMENT + ctx.Name().getFirst().getText();
-
+        var signature = ELEMENT + ":" + ctx.Name().getFirst().getText();
+        List<TechlandXMLParser.AttributeContext> attribute = ctx.attribute();
+        if (attribute != null) {
+            String attributeSignature = attribute.stream().map(RuleContext::getText).collect(Collectors.joining(":"));
+            signature += ":" + attributeSignature;
+        } else {
+            System.out.println("attribute = " + attribute);
+        }
+        TechlandXMLParser.ContentContext content = ctx.content();
+        if (content != null) {
+            List<TechlandXMLParser.ElementContext> element = content.element();
+        } else {
+            System.out.println("signature = " + signature);
+        }
         return super.visitElement(ctx);
     }
 }
